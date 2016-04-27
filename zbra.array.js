e0e062f3520060b4c46c32fdb3e10ee32e8492c2
Z.BRA - Perfil <zbraestudio@gmail.com>
2016-04-27 18:46:57 -0300

    Corrigindo

--- zbra.array.js
+++ zbra.array.js
@@ -2,18 +2,19 @@ zbra.array = new Object();
 
 /**
  * Verifica item-a-item de array se x valor já está contido neel.
- * @param array [array}
+ * @param array {array}
  * @param item
  * @returns {boolean}
  */
 zbra.array.search = function(array, item){
+  var r = false;
 
-    array.forEach(function(e){
+  array.forEach(function(e){
 
-        if(e == item)
-            return true;
-    });
+    if(e == item)
+      r = true;
+  });
 
-    return false;
+  return r;
 
 }
