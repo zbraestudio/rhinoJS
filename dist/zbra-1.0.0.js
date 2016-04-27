7de4d369bd54e8623beaa62650e7bd35109946bd
Tihh Gonçalves <tihh.gon@gmail.com>
2016-04-27 16:41:47 -0300

    Liberando versão 1.0.0

---
+++ dist/zbra-1.0.0.js
@@ -0,0 +1,107 @@
+var zbra = new Object();
+zbra.version = "1.0.0";
+
+/**
+ * Cria log no console
+ * @param msg
+ */
+zbra.log = function(msg){
+    console.log('Z.BRA Framework Javascript - ' + msg);
+}
+
+/**
+ * Exibe mensagens de erro do framework
+ * @param msg
+ */
+zbra.error = function(msg){
+    alert('Z.BRA Framework Javascript - ' + msg);
+
+    zbra.log('Parou!')
+}
+
+/**
+ * Faz verificações necessárias para um bom funcionamento do framework
+ * @returns {boolean}
+ */
+zbra.checking = function(){
+
+    //verifica se Jquery está carregado
+    if (typeof jQuery === 'undefined') {
+        zbra.error('o jQuery não foi carregado e é necessário.');
+        return false;
+    }
+
+    return true;
+}
+
+/**
+ * Carrega framework
+ */
+zbra.init = function(){
+
+    zbra.log('Iniciando...');
+
+    //verificações...
+    if(!zbra.checking()){
+        return;
+    }
+
+    zbra.log('Iniciado.');
+
+}
+
+//startando zbra!
+zbra.init();
+
+zbra.array = new Object();
+
+/**
+ * Verifica item-a-item de array se x valor já está contido neel.
+ * @param array [array}
+ * @param item
+ * @returns {boolean}
+ */
+zbra.array.search = function(array, item){
+
+    array.forEach(function(e){
+
+        if(e == item)
+            return true;
+    });
+
+    return false;
+
+}
+zbra.device = new Object();
+
+/**
+ * Verifica se o aparelho é acessado de um Celular
+ * @returns {boolean}
+ */
+zbra.device.isMobile = function(){
+
+    if( navigator.userAgent.match(/Android/i)
+        || navigator.userAgent.match(/webOS/i)
+        || navigator.userAgent.match(/iPhone/i)
+        || navigator.userAgent.match(/iPad/i)
+        || navigator.userAgent.match(/iPod/i)
+        || navigator.userAgent.match(/BlackBerry/i)
+        || navigator.userAgent.match(/Windows Phone/i)
+    )
+        return true;
+    else
+        return false;
+
+}/**
+ * Verifica se variável foi instanciada antes
+ * @param variavel
+ * @returns {boolean}
+ */
+zbra.isset = function(variavel){
+
+    if( (variavel === undefined) || (variavel === null) )
+        return false;
+    else
+        return true;
+
+}
