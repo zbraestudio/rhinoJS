00360fa15fc33f456a2af647b6438fa012078e40
Tihh Gonçalves <tihh.gon@gmail.com>
2016-05-10 15:13:53 -0300

    Corrigindo .bat

--- minificar.bat
+++ minificar.bat
@@ -1,1 +1,2 @@
-minify --output dist/rhinoJS.js rhinoJS.js Array.js Form.js navigator.js String.js validate.js
+cd development
+minify --output ../dist/rhinoJS.js rhinoJS.js Array.js Form.js navigator.js String.js validate.js
