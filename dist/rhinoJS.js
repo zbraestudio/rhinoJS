3b127ce4863ab73cfe4e9abda7ab67c59e5dff4d
Tihh Gonçalves <tihh.gon@gmail.com>
2017-08-11 12:21:05 -0300

    v1.3

--- dist/rhinoJS.js
+++ dist/rhinoJS.js
@@ -1,1 +1,54 @@
-var rhinoJS=new Object;rhinoJS.version="1.1.2.0",rhinoJS.log=function(a){console.log("rhinoJS Framework Javascript - "+a)},rhinoJS.error=function(a){alert("rhinoJS Framework Javascript - "+a),rhinoJS.log("Parou!")},rhinoJS.checking=function(){return"undefined"!=typeof jQuery||(rhinoJS.error("o jQuery não foi carregado e é necessário."),!1)},rhinoJS.init=function(){rhinoJS.log("Iniciando..."),rhinoJS.log("Fazendo verificação nas dependências de terceiros..."),rhinoJS.checking()&&rhinoJS.log("Terceiros verificados.")},rhinoJS.init();var rhinoJS_imgs_preload=new Array;Image.preload=function(a){if(rhinoJS_imgs_preload.push(a),rhinoJS_imgs_preload.length>0){jQuery("#rhinoJS_imgs_preload").remove();var b='<style id="rhinoJS_imgs_preload">body:after {content:';rhinoJS_imgs_preload.forEach(function(a){b+=" url("+a+")"}),b+=";display: none;}</style>",$(b).appendTo("head")}},navigator.isMobile=function(){return!!(navigator.userAgent.match(/Android/i)||navigator.userAgent.match(/webOS/i)||navigator.userAgent.match(/iPhone/i)||navigator.userAgent.match(/iPad/i)||navigator.userAgent.match(/iPod/i)||navigator.userAgent.match(/BlackBerry/i)||navigator.userAgent.match(/Windows Phone/i))},String.stringToNumbers=function(a){return a.replace(/[^\d]+/g,"")};var validate=new Object;validate.isset=function(a){return void 0!==a&&null!==a},validate.isCNPJ=function(a){if(a=String.stringToNumbers(a),""==a)return!1;if(14!=a.length)return!1;if("00000000000000"==a||"11111111111111"==a||"22222222222222"==a||"33333333333333"==a||"44444444444444"==a||"55555555555555"==a||"66666666666666"==a||"77777777777777"==a||"88888888888888"==a||"99999999999999"==a)return!1;for(tamanho=a.length-2,numeros=a.substring(0,tamanho),digitos=a.substring(tamanho),soma=0,pos=tamanho-7,i=tamanho;i>=1;i--)soma+=numeros.charAt(tamanho-i)*pos--,pos<2&&(pos=9);if(resultado=soma%11<2?0:11-soma%11,resultado!=digitos.charAt(0))return!1;for(tamanho+=1,numeros=a.substring(0,tamanho),soma=0,pos=tamanho-7,i=tamanho;i>=1;i--)soma+=numeros.charAt(tamanho-i)*pos--,pos<2&&(pos=9);return resultado=soma%11<2?0:11-soma%11,resultado==digitos.charAt(1)},validate.isCPF=function(a){var c,b=0;if(a=String.stringToNumbers(a),"00000000000"==a)return!1;for(i=1;i<=9;i++)b+=parseInt(a.substring(i-1,i))*(11-i);if(c=10*b%11,10!=c&&11!=c||(c=0),c!=parseInt(a.substring(9,10)))return!1;for(b=0,i=1;i<=10;i++)b+=parseInt(a.substring(i-1,i))*(12-i);return c=10*b%11,10!=c&&11!=c||(c=0),c==parseInt(a.substring(10,11))},validate.isMail=function(a){var b=/[^@\-\.\w]|^[_@\.\-]|[\._\-]{2}|[@\.]{2}|(@)[^@]*\1/,c=/@[\w\-]+\./,d=/\.[a-zA-Z]{2,3}$/;return a.search(b)==-1&&a.search(c)!=-1&&a.search(d)!=-1},validate.isObject=function(a){return"object"==typeof a},validate.isArray=function(a){return"object"==typeof a},validate.isString=function(a){return"string"==typeof a},validate.isNumber=function(a){return"number"==typeof a},validate.isBoolean=function(a){return"boolean"==typeof a},window.change=function(a){jQuery(document).ready(a),jQuery(window).resize(a)},Array.search=function(a,b){var c=!1;return a.forEach(function(a){a==b&&(c=!0)}),c};var Form=new Object;Form.send=function(a,b,c,d){b="undefined"!=typeof b?b:new Array,c="undefined"!=typeof c?c:"POST",d="undefined"!=typeof d?d:"_self";var e=$('<form action="#"></form>');e.attr("action",a),e.attr("method",c),e.attr("target",d),$.each(b,function(a,b){e.append('<input name="'+a+'" type="hidden" value="'+b+'">')}),$("body").append(e),e.submit(),e.remove()};
+var rhinoJS=new Object();rhinoJS.version="1.3beta";rhinoJS.log=function(msg){console.log('rhinoJS Framework Javascript - '+msg);}
+rhinoJS.error=function(msg){alert('rhinoJS Framework Javascript - '+msg);rhinoJS.log('Parou!')}
+rhinoJS.checking=function(){if(typeof jQuery==='undefined'){rhinoJS.error('o jQuery não foi carregado e é necessário.');return false;}
+return true;}
+rhinoJS.init=function(){rhinoJS.log('Iniciando...');rhinoJS.log('Fazendo verificação nas dependências de terceiros...');if(!rhinoJS.checking()){return;}
+rhinoJS.log('Terceiros verificados.');}
+rhinoJS.init();Array.search=function(array,item){var r=false;array.forEach(function(e){if(e==item)
+r=true;});return r;}
+var Form=new Object();Form.send=function(url,params,method,target){params=typeof params!=='undefined'?params:new Array();method=typeof method!=='undefined'?method:'POST';target=typeof target!=='undefined'?target:'_self';var form=$('<form action="#"></form>');form.attr('action',url);form.attr('method',method);form.attr('target',target);$.each(params,function(key,value){form.append('<input name="'+key+'" type="hidden" value="'+value+'">');});$('body').append(form);form.submit();form.remove();}
+navigator.isMobile=function(){if(navigator.userAgent.match(/Android/i)||navigator.userAgent.match(/webOS/i)||navigator.userAgent.match(/iPhone/i)||navigator.userAgent.match(/iPad/i)||navigator.userAgent.match(/iPod/i)||navigator.userAgent.match(/BlackBerry/i)||navigator.userAgent.match(/Windows Phone/i))
+return true;else
+return false;}
+navigator.realLink=function(url,link){if(url.substr(-1)=='/')
+url=url.substr(0,(url.length-1));var protocolo;if(url.substr(0,5)=='https')
+protocolo='https';else
+protocolo='http'
+url=url.substr(protocolo.length+3);var dominio;var url_niveis=url.split('/');dominio=url_niveis.shift();if(link.indexOf('://')>=0)
+return link;if(link.substr(0,2)=='//'){return protocolo+':'+link;}
+if(link.substr(0,1)=='/'){return protocolo+'://'+dominio+'/'+link.substr(1);}
+var link_niveis=link.split('/');if(link.substr(0,3)=='../'||link.substr(0,5)=='./../'){var retornos_n=0;link_niveis.forEach(function(item){if(item=='.')
+retornos_n++;if(item=='..'){retornos_n++;url_niveis.pop();}});link_niveis=link_niveis.slice(retornos_n);if(url_niveis.length>0)
+return protocolo+'://'+dominio+'/'+url_niveis.join('/')+'/'+link_niveis.join('/');else
+return protocolo+'://'+dominio+'/'+link_niveis.join('/');}
+if(link.substr(0,2)=='./'){link=link.substr(2);}
+return protocolo+'://'+dominio+'/'+url_niveis.join('/')+'/'+link;}
+String.stringToNumbers=function(text){return text.replace(/[^\d]+/g,'');}
+var validate=new Object();validate.isset=function(variavel){if((variavel===undefined)||(variavel===null))
+return false;else
+return true;}
+validate.isCNPJ=function(cnpj){cnpj=String.stringToNumbers(cnpj);if(cnpj=='')return false;if(cnpj.length!=14)
+return false;if(cnpj=="00000000000000"||cnpj=="11111111111111"||cnpj=="22222222222222"||cnpj=="33333333333333"||cnpj=="44444444444444"||cnpj=="55555555555555"||cnpj=="66666666666666"||cnpj=="77777777777777"||cnpj=="88888888888888"||cnpj=="99999999999999")
+return false;tamanho=cnpj.length-2
+numeros=cnpj.substring(0,tamanho);digitos=cnpj.substring(tamanho);soma=0;pos=tamanho-7;for(i=tamanho;i>=1;i--){soma+=numeros.charAt(tamanho-i)*pos--;if(pos<2)
+pos=9;}
+resultado=soma%11<2?0:11-soma%11;if(resultado!=digitos.charAt(0))
+return false;tamanho=tamanho+1;numeros=cnpj.substring(0,tamanho);soma=0;pos=tamanho-7;for(i=tamanho;i>=1;i--){soma+=numeros.charAt(tamanho-i)*pos--;if(pos<2)
+pos=9;}
+resultado=soma%11<2?0:11-soma%11;if(resultado!=digitos.charAt(1))
+return false;return true;}
+validate.isCPF=function(cpf){var soma=0;var resto;cpf=String.stringToNumbers(cpf);if(cpf=="00000000000")return false;for(i=1;i<=9;i++)soma=soma+parseInt(cpf.substring(i-1,i))*(11-i);resto=(soma*10)%11;if((resto==10)||(resto==11))
+resto=0;if(resto!=parseInt(cpf.substring(9,10)))
+return false;soma=0;for(i=1;i<=10;i++)
+soma=soma+parseInt(cpf.substring(i-1,i))*(12-i);resto=(soma*10)%11;if((resto==10)||(resto==11))
+resto=0;if(resto!=parseInt(cpf.substring(10,11)))
+return false;return true;}
+validate.isMail=function(email){var exclude=/[^@\-\.\w]|^[_@\.\-]|[\._\-]{2}|[@\.]{2}|(@)[^@]*\1/;var check=/@[\w\-]+\./;var checkend=/\.[a-zA-Z]{2,3}$/;if(((email.search(exclude)!=-1)||(email.search(check))==-1)||(email.search(checkend)==-1)){return false;}
+else{return true;}}
+validate.isObject=function(val){return(typeof val=='object');}
+validate.isArray=function(val){return(typeof val=='object');}
+validate.isString=function(val){return(typeof val=='string');}
+validate.isNumber=function(val){return(typeof val=='number');}
+validate.isBoolean=function(val){return(typeof val=='boolean');}
+var rhinoJS_imgs_preload=new Array();Image.preload=function(url){rhinoJS_imgs_preload.push(url);if(rhinoJS_imgs_preload.length>0){jQuery('#rhinoJS_imgs_preload').remove();var css_script='<style id="rhinoJS_imgs_preload">body:after {content:';rhinoJS_imgs_preload.forEach(function(e){css_script+=' url('+e+')';})
+css_script+=';display: none;}</style>';$(css_script).appendTo('head');}}
