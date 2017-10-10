bd8add8adb169961d51090751c963fee45901814
Tihh Gonçalves <tihh.gon@gmail.com>
2017-10-10 12:03:07 -0300

    Adicionando o arquivo window.js no controle de minificar

--- minificar.bat
+++ minificar.bat
@@ -1,3 +1,3 @@
 cd src
-minify --output ../dist/rhinoJS.js rhinoJS.js Array.js Form.js navigator.js String.js validate.js Image.js
+minify --output ../dist/rhinoJS.js rhinoJS.js Array.js Form.js navigator.js String.js validate.js Image.js window.js
 PAUSE
